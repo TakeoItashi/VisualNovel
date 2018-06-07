beca18b332cf86aef15a6d868a864bb19f46f00a
TomAppel Laptop <tom.appel@web.de>
2018-06-07 17:20:16 +0200

    -Implemented basic Textloader class -Reworked the Storyboard File

---
+++ VisualNovel/TextLoader.cpp
@@ -0,0 +1,18 @@
+#include "TextLoader.h"
+
+TextLoader::TextLoader() {
+}
+
+
+TextLoader::~TextLoader() {
+}
+
+void TextLoader::LoadText() {
+
+	//Parse ImageImports.txt
+	std::ifstream imagefile("ImageImports.txt");
+	std::string fileNames;
+	int imageCount = 0;
+
+	}
+}
---
+++ VisualNovel/TextLoader.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <fstream>
+#include <sstream>
+#include <string>
+
+class TextLoader {
+public:
+	TextLoader();
+	~TextLoader();
+
+	void LoadText();
+};
+
