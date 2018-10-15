0b074da928251e7a19231417a0014266ece724b1
TomAppel Laptop <tom.appel@web.de>
2018-10-15 16:17:09 +0200

    -Small changes to the settings.h: added Screen size variables

--- VisualNovel/Settings.h
+++ VisualNovel/Settings.h
@@ -8,8 +8,8 @@ class Settings {
 		~Settings();
 
 		//Screen Variables
-
+		int m_WindowWidth, m_WindowHeight, m_windowPosX, m_windowPosY;
 
 		//TODO richtige implementierung
-		std::string TextBoxColor, TextBoxSpeed, SkipSettings, TextBoxAlpha, TextBoxTextColor,SoundSettings, ScreenSettings;
+		std::string TextBoxColor, TextBoxSpeed, SkipSettings, TextBoxAlpha, TextBoxTextColor, SoundSettings, ScreenSettings;
 };
