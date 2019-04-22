741f1ab71b6ad067a6db24bfd4363b759b9fbadd
TomAppel-PC <tom.appel@web.de>
2019-04-23 01:07:57 +0200

    -Started on the Basic Layout of the Interface

---
+++ VisualNovelInterface/MVVM/BaseModelView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualNovelInterface.MVVM
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BaseViewModel()
+        {
+
+        }
+        
+        protected void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ VisualNovelInterface/TestViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualNovelInterface.MVVM;
+
+namespace VisualNovelInterface
+{
+    public class TestViewModel : BaseViewModel
+    {
+        string stringProperty;
+
+        public string StringProperty {
+            get => stringProperty;
+            set {
+                stringProperty = value;
+                OnPropertyChanged(nameof(stringProperty));
+            }
+        }
+    }
+}
